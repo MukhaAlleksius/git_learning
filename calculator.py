843957b33a11c4e377646769d07a58a4637be6dd
Sasha <mukha.alleksius@mail.ru>
2024-10-26 22:51:29 +0300

    Шаблон калькулятора

---
+++ calculator.py
@@ -0,0 +1,9 @@
+class Calculator:
+    last_res = None
+
+    def sum(self, n1, n2):
+        self.last_res = n1 + n2
+        return self.last_res
+
+    def print_last_res(self):
+        print(self.last_res)
